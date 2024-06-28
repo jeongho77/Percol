ff2f500241bc3b73f3351d74cffdfd1c32581a43
qbnyp <moneycloudy@naver.com>
2024-06-28 20:34:23 +0900

    Controller login, 메인화면 추가

---
+++ src/main/java/Umc/Percol/controller/LoginController.java
@@ -0,0 +1,32 @@
+package Umc.Percol.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/login")
+public class LoginController {
+
+    //로그인화면 ( 카카오 , 네이버 선택)
+    @GetMapping("/")
+    public String login(){
+        return "login";
+    }
+
+    //카카오 로그인 뷰
+    @GetMapping("/kakao")
+    public String kakaoLogin(){
+        return "kakao-login";
+    }
+
+    //네이버 로그인 뷰
+    @GetMapping("/naver")
+    public String naverLogin(){
+        return "naver-login";
+    }
+
+}
